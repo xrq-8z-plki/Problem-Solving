f5182e22491819c78ae0b5d0c92b8d67a17420b9
Shalu <shlokashalini@gmail.com>
2025-11-26 21:37:22 +0530

    Added subarraySum Solution

---
+++ Java/subarraySum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int[] sum=new int[nums.length];
+        sum[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            sum[i]=sum[i-1]+nums[i];
+        }
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(sum[i]==k) count++;
+            for(int j=0;j<i;j++){
+                if(sum[i]-sum[j]==k) count++;
+            }
+        }
+        return count;
+    }
+}
