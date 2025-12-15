e32ce3171d377e0220bb155fe340dbd30a58854f
Shalu <shlokashalini@gmail.com>
2025-12-15 21:27:37 +0530

    Added getDescentPeriods Solution

---
+++ Java/getDescentPeriods.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long ans = 1;
+        long count = 1;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] == prices[i + 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+            ans = ans + count;
+        }
+        return ans;
+    }
+}
