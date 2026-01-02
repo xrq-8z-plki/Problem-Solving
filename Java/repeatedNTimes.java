9b68f87927556c1f3ed2525516ddb647a49c1dc1
Shalu <shlokashalini@gmail.com>
2026-01-02 16:37:27 +0530

    Added repeatedNTimes Solution

---
+++ Java/repeatedNTimes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
